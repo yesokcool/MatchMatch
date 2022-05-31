c6aaba0a5c88c6d97802e28f24ad0811f1b3c83a
yesokcool <ysokcl@gmail.com>
2022-05-31 18:14:40 -0400

    Add deck dealing animation

--- Match/SymbolMatchGameView.swift
+++ Match/SymbolMatchGameView.swift
@@ -8,60 +8,115 @@ import SwiftUI
 struct SymbolMatchGameView: View {
     @ObservedObject var game: SymbolMatchGame
     
+    @Namespace private var dealingNamespace
+    
     var body: some View {
         VStack {
-            VStack {
-                Text(game.getThemeName())
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(game.getThemeColor())
-                Text("SCORE: \(game.getScore())")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                HStack {
-                    Text("Guesses: \(game.getNumberOfGuesses())")
-                    Text("Modifier: \(game.getScoreModifier())")
-                }
-                .font(.subheadline)
-            }
             
+            gameTitle
             gameBody
+            deckBody
             shuffle
-            
-            HStack(alignment: .top) {
-                Button {
-                    game.newGame()
-                } label: {
-                    VStack(alignment: .center) {
-                        Image(systemName: "gamecontroller.fill")
-                        Text ("New Game")
-                            .font(.caption)
-                    }
-                }
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
-            .foregroundColor(.blue)
+            gameControls
             
             Spacer()
+        }
+    }
+    
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: SymbolMatchGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: SymbolMatchGame.Card) -> Bool {
+        !dealt.contains(card.id)
+    }
+        
+    var gameTitle: some View {
+        VStack {
+            Text(game.getThemeName())
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(game.getThemeColor())
+            Text("SCORE: \(game.getScore())")
+                .font(.title3)
+                .fontWeight(.semibold)
+            HStack {
+                Text("Guesses: \(game.getNumberOfGuesses())")
+                Text("Modifier: \(game.getScoreModifier())")
+            }
+            .font(.subheadline)
         }
         .foregroundColor(game.getThemeColor())
         .padding()
     }
-        
+    
+    var gameControls: some View {
+        HStack(alignment: .top) {
+            Button {
+                game.newGame()
+            } label: {
+                VStack(alignment: .center) {
+                    Image(systemName: "gamecontroller.fill")
+                    Text ("New Game")
+                        .font(.caption)
+                }
+            }
+        }
+        .font(.largeTitle)
+        .padding(.horizontal)
+        .foregroundColor(.blue)
+    }
+    
+    private func dealAnimation(for card: SymbolMatchGame.Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id }) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+    
+    private func zIndex(of card: SymbolMatchGame.Card) -> Double {
+        -Double(game.cards.firstIndex(where: { $0.id == card.id }) ?? 0)
+    }
+    
     var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
                 Color.clear
             }
             else {
                 CardView(card: card, theme: game.getThemeColor())
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
+                    .transition(AnyTransition.asymmetric(insertion: .identity, removal: .opacity))
+                    .zIndex(zIndex(of: card))
                     .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.5)) {
+                        withAnimation {
                             game.choose(card)
                         }
                     }
+            }
+        }
+    }
+    
+    var deckBody: some View {
+        ZStack {
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card: card, theme: game.getThemeColor())
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+            }
+        }
+        .frame(width: CardConstants.undealtWidth, height: CardConstants.undealtHeight)
+        .foregroundColor(game.getThemeColor())
+        .onTapGesture {
+            // deal cards
+            for card in game.cards {
+                withAnimation(dealAnimation(for: card)) {
+                    deal(card)
+                }
             }
         }
         .padding(.horizontal)
@@ -73,6 +128,16 @@ struct SymbolMatchGameView: View {
                 game.shuffle()
             }
         }
+    }
+    
+    private struct CardConstants {
+        static let color = Color.red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDealDuration: Double = 2
+        static let undealtHeight: CGFloat = 90
+        static let undealtWidth = undealtHeight * aspectRatio
+        
     }
 }
 
